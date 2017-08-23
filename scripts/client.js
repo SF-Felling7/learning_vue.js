a7d2ed984bb0069fd36d7cf6726ab33f8465c55f
Sean Felling <Snfelling7@gmail.com>
2017-08-23 14:37:50 -0500

    added new h1 tag

--- scripts/client.js
+++ scripts/client.js
@@ -2,6 +2,7 @@ new Vue({
   el:'#app',
   data:{
     message:'Hello World!',
-    message3: 'Going to travel the world!'
+    message3: 'Going to travel the world!',
+    message5: 'At a coffee shop'
   }
 });
