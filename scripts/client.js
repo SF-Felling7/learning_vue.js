30e84a90af086ebcdd2c6d0712cc527f76fa3cfe
Sean Felling <Snfelling7@gmail.com>
2017-08-29 18:53:18 -0500

    added message

--- scripts/client.js
+++ scripts/client.js
@@ -4,6 +4,7 @@ new Vue({
     message:'Hello World!',
     message3: 'Going to travel the world!',
     message5: 'At a coffee shop',
-    message7: 'Feeling great!'
+    message7: 'Feeling great!',
+    message10: 'hopefully getting a job soon!'
   }
 });
