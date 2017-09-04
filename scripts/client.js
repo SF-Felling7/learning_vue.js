2cd4a01350e002d3c65d7b11423e08eb77032e51
Sean Felling <Snfelling7@gmail.com>
2017-09-04 16:19:56 -0500

    hello world

--- scripts/client.js
+++ scripts/client.js
@@ -1,10 +1,6 @@
 new Vue({
   el:'#app',
   data:{
-    message:'Hello World!',
-    message3: 'Going to travel the world!',
-    message5: 'At a coffee shop',
-    message7: 'Feeling great!',
-    message10: 'hopefully getting a job soon!'
+    message:'Hello World!'
   }
 });
